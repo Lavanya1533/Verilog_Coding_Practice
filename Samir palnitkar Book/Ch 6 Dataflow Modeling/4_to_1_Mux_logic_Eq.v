3b3661a8e08f07f8cbfebb1410977b769958b5c7
deoregaurav92 <deoregaurav92@gmail.com>
2021-06-13 16:51:47 -0700

    4:1 MUX using logic equation

--- Samir palnitkar Book/Ch 6 Dataflow Modeling/4_to_1_Mux_logic_Eq.v
+++ Samir palnitkar Book/Ch 6 Dataflow Modeling/4_to_1_Mux_logic_Eq.v
@@ -4,4 +4,10 @@ module mux(sel, IN, OUT);
   input [3:0] IN;
   output OUT;
 
-  assign OUT = ()
+//Continuous assignment using logical operators
+
+  assign OUT = ((!sel[1] && !sel[0] && IN[0]) ||
+                (!sel[1] && sel[0] && IN[1]) ||
+                (sel[1] && !sel[0] && IN[2]) ||
+                (sel[1] && sel[0] && IN[3]));
+endmodule
