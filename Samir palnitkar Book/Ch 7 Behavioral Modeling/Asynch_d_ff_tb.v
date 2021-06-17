5ccf754483346d43cb48a7dc563c7bb54442b4c8
deoregaurav92 <deoregaurav92@gmail.com>
2021-06-16 18:08:22 -0700

    Testbench of active high asynch reset D_FF

---
+++ Samir palnitkar Book/Ch 7 Behavioral Modeling/Asynch_d_ff_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module asyncd_ff_tb();
+  reg D;
+  reg CLK, RST;
+  wire Q;
+
+  //Top-level module instantiatation
+  asynch_d_ff d1(.d(D), .clk(CLK), .rst(RST), .q(Q));
+
+  initial
+  begin
+      CLK = 1'b0;
+      RST = 1'b1;
+      D   = 1'b1;
+  #3  RST = 1'b0;
+  #3  D   = 1'b0;
+  #3  D   = 1'b1;
+  #3  D   = 1'b0;
+  #10 $stop;
+  end
+
+  always
+  begin
+    forever #5 CLK = ~CLK;
+  end
+
+  initial begin
+    $monitor($time, "Reset = %b, Data = %b, Output = %b", RST, D, Q);
+  end
+endmodule
