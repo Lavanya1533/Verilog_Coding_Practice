692522283405d28b16282f3f82dfea5d57288340
deoregaurav92 <deoregaurav92@gmail.com>
2021-06-16 18:08:47 -0700

    Active high synch reset D_FF

--- Samir palnitkar Book/Ch 7 Behavioral Modeling/d_ff.v
+++ Samir palnitkar Book/Ch 7 Behavioral Modeling/d_ff.v
@@ -2,7 +2,9 @@ module d_ff(  input d,
               input clk, rst,
               output reg q);
 
-  always @ ( negedge clk ) begin
+//Active high synchronous D Flip Flop
+
+  always @ (negedge clk) begin
     if (rst)
       q <= 1'b0;
     else
